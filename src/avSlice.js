de85013a373e9f94b2b6a0ad1821bfd341bb6330
homemadejam16 <homemadejam16@gmail.com>
2025-08-10 11:59:52 -0400

    message

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,15 +3,52 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+      img: "https://pixabay.com/photos/speakers-bluetooth-tech-speaker-4109274/
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/photos/business-computer-conference-20031/",
+      name: "projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/photos/public-speaking-mic-microphone-3926344/",
+      name: "microphones",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/photos/whiteboard-dry-erase-marker-blank-2903269/",
+      name: "whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/photos/signpost-waypoint-wood-grain-board-235079/",
+      name: "signage",
+      cost: 80,
+      quantity: 0,
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
+        const items = state[action.payload];
+        if(items) {
+            items.quantity++;
+        };
       
     },
     decrementAvQuantity: (state, action) => {
+        const items = state[action.payload];
+        if (items && items > 0){
+            items.quantity--;
+
+        };
      
     },
   },
--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,10 +4,31 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    {
+      name: "breakfast",
+      cost: 50,
+      selected: false,
+    },
+    {
+      name: "high tea",
+      cost: 25,
+      selected: false,
+    },
+    {
+      name: "lunch",
+      cost: 65,
+      selected: false,
+    },
+    {
+      name: dinner,
+      cost: 70,
+      selected: false,
+    },
   ],
+
   reducers: {
     toggleMealSelection: (state, action) => {
+        state[action.payload].selected = !state[action.payload].selected;
     },
   },
 });
